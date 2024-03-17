731ed15cf30ec03626cbd428e9c641559695baea
GeorgyKA31P <gorgn2005@gmail.com>
2024-03-17 23:01:39 +0300

    End create appearance StartProgramScreen. Create fun InstallationBackgroundColumn

--- app/src/main/java/com/example/smartlab/MainActivity.kt
+++ app/src/main/java/com/example/smartlab/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.smartlab.screens.StartProgramScreen
 import com.example.smartlab.ui.theme.SmartLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    StartProgramScreen()
                 }
             }
         }
--- app/src/main/java/com/example/smartlab/screens/StartProgramScreen.kt
+++ app/src/main/java/com/example/smartlab/screens/StartProgramScreen.kt
@@ -1,11 +1,34 @@
 package com.example.smartlab.screens
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.smartlab.R
 import kotlinx.coroutines.delay
+import org.w3c.dom.Text
 
 @Composable
 fun SwitchingThePage(navController: NavHostController) {
@@ -25,7 +48,52 @@ fun SwitchingThePage(navController: NavHostController) {
     }
 }
 
+fun InstallationBackgroundColumn(): Brush {
+    return Brush.linearGradient(
+        colors = listOf(
+            Color(android.graphics.Color.parseColor("#70bce3")),
+            Color(android.graphics.Color.parseColor("#5c97e0")),
+            Color(android.graphics.Color.parseColor("#517de4")),
+
+            Color(android.graphics.Color.parseColor("#3f66ea")),
+            Color(android.graphics.Color.parseColor("#2856f2")),
+            Color(android.graphics.Color.parseColor("#2856f2")),
+            Color(android.graphics.Color.parseColor("#3f66ea")),
+            Color(android.graphics.Color.parseColor("#517de4")),
+            Color(android.graphics.Color.parseColor("#5c97e0")),
+            Color(android.graphics.Color.parseColor("#70bce3")),
+        )
+    )
+}
+git commit -m "End create appearance StartProgramScreen. Create fun"
 @Composable
-fun StartProgramScreen(navController: NavHostController) {
-    SwitchingThePage(navController)
+fun StartProgramScreen() {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxHeight()
+            .background(brush = InstallationBackgroundColumn())
+    ){
+        Row {
+            Text(
+                text = "Смартлаб",
+                fontSize = 30.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+            Image(
+                bitmap = ImageBitmap.imageResource(R.drawable.shape),
+                contentDescription = "Иконка",
+                modifier = Modifier
+                    .size(45.dp, 40.dp)
+                    .padding(start=10.dp)
+
+            )
+        }
+
+    }
+    //SwitchingThePage(navController)
+
 }
