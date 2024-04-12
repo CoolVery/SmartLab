47eed1f181fb6415d51d89a30ac70b87289bdec0
CoolVery <georgeclgit@gmail.com>
2024-04-12 22:13:20 +0300

    Eighth step. Send code by email. Create viewmodel provider in MainActivity

--- app/src/main/java/com/example/smartlab/MainActivity.kt
+++ app/src/main/java/com/example/smartlab/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.smartlab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,11 +11,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.smartlab.api.RepositoryImpl
+import com.example.smartlab.api.RetrofitInstance
 import com.example.smartlab.navigation.Navigation
 import com.example.smartlab.screens.StartProgramScreen
 import com.example.smartlab.ui.theme.SmartLabTheme
+import com.example.smartlab.viewmodelmain.ViewModelMain
 
 class MainActivity : ComponentActivity() {
+    private val viewModelSmart by viewModels<ViewModelMain>(factoryProducer = {
+        object : ViewModelProvider.Factory {
+            override  fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return ViewModelMain(RepositoryImpl(RetrofitInstance.apiSmartLab)) as T
+            }
+        }
+    })
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
