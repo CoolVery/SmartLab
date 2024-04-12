67137117ce008a0f14619f34a330511a8041a00f
CoolVery <georgeclgit@gmail.com>
2024-04-12 20:17:38 +0300

    Add converter-scalars for read code in Api

--- app/src/main/java/com/example/smartlab/Api/Repository.kt
+++ app/src/main/java/com/example/smartlab/Api/Repository.kt
@@ -3,5 +3,6 @@ package com.example.smartlab.Api
 import kotlinx.coroutines.flow.Flow
 
 interface Repository {
+    //Fun from ApiInter.This fun try-catch override in RepositoryApi interface
     suspend fun sendCodeEmail(email: String):Flow<Result<String>>
 }
